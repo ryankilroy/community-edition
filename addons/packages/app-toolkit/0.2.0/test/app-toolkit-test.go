6df1801b2cf4b445bfa42842da9cde7b9e7d335a
Ryan Collins <rycollins@vmware.com>
2022-04-19 14:40:31 -0400

    remove http prefix

--- addons/packages/app-toolkit/0.2.0/test/app-toolkit-test.go
+++ addons/packages/app-toolkit/0.2.0/test/app-toolkit-test.go
@@ -42,7 +42,7 @@ func main() {
 
 	fmt.Println("\nTEST STEP Prepare Secrets")
 	//TODO: remove when we can use the official package repository
-	runCommand("tanzu package repository update projects.registry.vmware.com-tce-main-v0.11.0 -n tanzu-package-repo-global --url http://index.docker.io/ryanmattcollins/main@sha256:52da5141d10490fa755fb90e56141653160d8af2a9c0b3a37bf1acfd802cfc8b")
+	runCommand("tanzu package repository update projects.registry.vmware.com-tce-main-v0.11.0 -n tanzu-package-repo-global --url index.docker.io/ryanmattcollins/main@sha256:52da5141d10490fa755fb90e56141653160d8af2a9c0b3a37bf1acfd802cfc8b")
 	runCommand("tanzu package install secretgen-controller --package-name secretgen-controller.community.tanzu.vmware.com --version 0.8.0")
 	runCommand("export REGISTRY_SERVER=$(grep server: app-toolkit-values.yaml | awk '{print $2}')")
 	runCommand("export REGISTRY_USER=$(grep kp_default_repository_username: app-toolkit-values.yaml | awk '{print $2}')")
